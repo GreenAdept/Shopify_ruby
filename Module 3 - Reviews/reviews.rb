3ff07839358ba0dad441c0ad74fbe451525ff8b6
Robert Hilton <greenadept@gmail.com>
2021-06-26 17:30:03 -0600

    Eliminating elements we don't want.

--- Module 3 - Reviews/reviews.rb
+++ Module 3 - Reviews/reviews.rb
@@ -4,7 +4,7 @@ File.open("reviews.txt") do |review_file|
     lines = review_file.readlines
 end
 
-puts lines.length
-
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
-puts relevant_lines
+reviews = relevant_lines.reject { |line| line.include?("--")}
+
+puts reviews
